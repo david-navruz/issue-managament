310c6afc414548d7f6e9c2a2242ac05ade1c83d2
David <Seltesla84>
2021-06-06 23:09:53 +0200

    update

---
+++ src/main/java/com/udemy/issuemanagement/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.udemy.issuemanagement.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    private String username;
+    private String password;
+
+}
---
+++ src/main/java/com/udemy/issuemanagement/service/IssueService.java
@@ -0,0 +1,16 @@
+package com.udemy.issuemanagement.service;
+
+import com.udemy.issuemanagement.entity.Issue;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IssueService {
+
+    Issue save(Issue issue);
+
+    Issue getById(Long id);
+
+    Page<Issue> getAllPageable(Pageable pageable);
+
+
+}
