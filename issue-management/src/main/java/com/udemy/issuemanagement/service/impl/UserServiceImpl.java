983be70515301f16d29eae50f6fd4a4a582f5a9a
david-navruz <selcuk-nevruz@hotmail.com>
2021-06-13 01:29:38 +0200

    update

---
+++ issue-management/src/main/java/com/udemy/issuemanagement/service/impl/UserServiceImpl.java
@@ -0,0 +1,86 @@
+package com.udemy.issuemanagement.service.impl;
+
+import com.udemy.issuemanagement.dto.RegistrationRequest;
+import com.udemy.issuemanagement.dto.UserDto;
+import com.udemy.issuemanagement.entity.User;
+import com.udemy.issuemanagement.repository.UserRepository;
+import com.udemy.issuemanagement.service.UserService;
+import com.udemy.issuemanagement.util.TPage;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public UserDto save(UserDto user) {
+        User u = modelMapper.map(user, User.class);
+        u = userRepository.save(u);
+        user.setId(u.getId());
+        return user;
+    }
+
+    @Override
+    public UserDto getById(Long id) {
+        User u = userRepository.getOne(id);
+        return modelMapper.map(u, UserDto.class);
+    }
+
+    @Override
+    public TPage<UserDto> getAllPageable(Pageable pageable) {
+        Page<User> data = userRepository.findAll(pageable);
+        TPage<UserDto> response = new TPage<UserDto>();
+        response.setStat(data, Arrays.asList(modelMapper.map(data.getContent(), UserDto[].class)));
+        return response;
+    }
+
+    @Override
+    public UserDto getByUsername(String username) {
+        User u = userRepository.findByUsername(username);
+        return modelMapper.map(u, UserDto.class);
+    }
+
+    public List<UserDto> getAll() {
+        List<User> data = userRepository.findAll();
+        return Arrays.asList(modelMapper.map(data, UserDto[].class));
+    }
+
+    @Transactional
+    public Boolean register(RegistrationRequest registrationRequest) {
+        try {
+            User user = new User();
+            user.setEmail(registrationRequest.getEmail());
+            user.setNameSurname(registrationRequest.getNameSurname());
+            user.setPassword(bCryptPasswordEncoder.encode(registrationRequest.getPassword()));
+            user.setUsername(registrationRequest.getUsername());
+            userRepository.save(user);
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            log.error("REGISTRATION=>", e);
+            return Boolean.FALSE;
+        }
+    }
+
+
+}
