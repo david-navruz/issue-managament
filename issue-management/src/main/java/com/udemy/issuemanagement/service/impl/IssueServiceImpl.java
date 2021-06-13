37bf4025d72972ff201e96c4c07ec2c875683523
david-navruz <selcuk-nevruz@hotmail.com>
2021-06-13 21:09:47 +0200

    update

--- issue-management/src/main/java/com/udemy/issuemanagement/service/impl/IssueServiceImpl.java
+++ issue-management/src/main/java/com/udemy/issuemanagement/service/impl/IssueServiceImpl.java
@@ -6,8 +6,9 @@ import com.udemy.issuemanagement.service.IssueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class IssueServiceImpl implements IssueService {
 
     private IssueRepository issueRepository;
