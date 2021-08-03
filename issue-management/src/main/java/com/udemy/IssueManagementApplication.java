831c904001fec5c1f6b3b0490766bef1898312c2
david-navruz <selcuk-nevruz@hotmail.com>
2021-08-03 22:24:22 +0200

    update project

--- issue-management/src/main/java/com/udemy/IssueManagementApplication.java
+++ issue-management/src/main/java/com/udemy/IssueManagementApplication.java
@@ -1,9 +1,14 @@
 package com.udemy;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @SpringBootApplication
 public class IssueManagementApplication {
@@ -14,7 +19,18 @@ public class IssueManagementApplication {
 
 	@Bean
 	public ModelMapper getModelMapper(){
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		return new ModelMapper();
 	}
+
+	@Bean
+	public Jackson2RepositoryPopulatorFactoryBean repositoryPopulatorFactoryBean() {
+		Jackson2RepositoryPopulatorFactoryBean factoryBean = new Jackson2RepositoryPopulatorFactoryBean();
+		factoryBean.setResources(new Resource[] {new ClassPathResource("projects.json")} );
+		return factoryBean;
+	}
+
+
 
 }
---
+++ issue-management/src/main/java/com/udemy/issuemanagement/advice/ExceptionResponse.java
@@ -0,0 +1,19 @@
+package com.udemy.issuemanagement.advice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+
+    private Date date;
+    private String message;
+
+
+
+
+}
